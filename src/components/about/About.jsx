2be719def4a901e49c139e688201e4e67b562a98
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-02-25 22:17:33 -0500

    made personal website from tutorial

---
+++ src/components/about/About.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import './about.css'
+import myImage from '../../assets/Headshot.jpg'
+import {GiGraduateCap} from 'react-icons/gi'
+import {BsBookmarkStar} from 'react-icons/bs'
+import {TfiWorld} from 'react-icons/tfi'
+
+const About = () => {
+  return (
+    <section id='about'>
+      
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+
+          <div className="about__me">
+            <div className="about__me-image">
+                <img src={myImage} alt="Me" />
+            </div>
+          </div>
+
+          <div className="about__content">
+            <div className="about__cards">
+
+              <article className='about__card'>
+                  <GiGraduateCap className='about__icon'/>
+                  <h5>Degree</h5>
+                  <small>B.S. Computer Science <br /><i>University of Georgia</i></small>
+              </article>
+
+              <article className='about__card'>
+                  <BsBookmarkStar className='about__icon'/>
+                  <h5>Current GPA</h5>
+                  <small>3.47</small>
+              </article>
+
+              <article className='about__card'>
+                  <TfiWorld className='about__icon'/>
+                  <h5>Domains</h5>
+                  <small>
+                    <ul>
+                      <li>Web Applications</li>
+                      <li>Full-stack</li>
+                      <li>Business</li>
+                    </ul>
+                  </small>
+              </article>
+
+              
+
+            </div>
+            <p>
+            Hello! I'm Tony Nguyen, a Computer Science major with a Business Minor at The University of Georgia, on track to graduate in May 2025. 
+            I'm on the lookout for internships, co-ops, and full-time opportunities to dive into the dynamic world of technology.
+            My passion lies in transforming ideas into reality through code. I've actively participated in Hackathons, 
+            where I've crafted applications using technologies like <b> React, Node.js, HTML, CSS, JavaScript, Flutter, and Dart </b>. On the backend, 
+            I've worked with <b>Python, MongoDB, and MySQL,</b> contributing to well-rounded, full-stack development    
+            </p>
+
+          </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/myprojects/Myprojects.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import './myprojects.css'
+import IMG1 from '../../assets/MusicMe.png'
+import IMG2 from '../../assets/ArtistSearch.jpeg'
+import IMG3 from '../../assets/StyleCast.png'
+import IMG4 from '../../assets/ChefItUp.png'
+import IMG5 from '../../assets/ComicCache.png'
+
+const Myprojects = () => {
+  return (
+    <section id='myprojects'>
+
+      <h5>My Recent Works</h5>
+      <h2>Projects</h2>
+
+      <div className="container portfolio__container">
+
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG1} alt="" />
+            </div>
+            <h3>Music Me</h3>
+            <small className='text-light'>Flutter | Dart | Xcode </small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/TonyQNguy/musicme" target="_blank" rel='noreferrer' className='btn'>Github</a>
+            </div>
+              
+        </article>
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG4} alt="" />
+            </div>
+            <h3>Chef It Up</h3>
+            <small className='text-light'>Flutter | Yummly API | Dart | Xcode</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/TonyQNguy/chefitup" target="_blank" rel='noreferrer' className='btn'>Github</a>
+            </div>
+            
+        </article>
+
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG5} alt="" />
+            </div>
+            <h3>Comic Cache</h3>
+            <small className='text-light'>Flutter | Dart | Xcode</small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/TonyQNguy/comiccacheapp" target="_blank" rel='noreferrer' className='btn'>Github</a>
+            </div>
+            
+        </article>
+        
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG3} alt="" />
+            </div>
+            <h3>StyleCast</h3>
+            <small className='text-light'> ReactJS | HTML | CSS | JavaScript </small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/TonyQNguy/stylecast" target="_blank" rel='noreferrer' className='btn'>Github</a>
+            </div>
+            
+        </article>
+
+        <article className='portfolio__item'>
+            <div className="portfolio__item-image">
+                <img src={IMG2} alt="" />
+            </div>
+            <h3>Artist Search</h3>
+            <small className='text-light'>Python | Spotify Developer API </small>
+            <div className="portfolio__item-cta">
+              <a href="https://github.com/TonyQNguy/ArtistSearch" target="_blank" rel='noreferrer' className='btn'>Github</a>  
+            </div>
+            
+        </article>
+
+      </div>
+    </section>
+  )
+}
+
+export default Myprojects
